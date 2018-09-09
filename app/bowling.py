c4097a51341dfc63198b08b14be43ecdfe60dee4
Vincent Auguin <v.auguin@gmail.com>
2018-09-10 00:54:10 +0200

    finalized the open , spare and strike frames

--- app/bowling.py
+++ app/bowling.py
@@ -1,5 +1,72 @@
+from abc import ABCMeta, abstractmethod
 
 class BowlingGame(object):
 
     def __init__(self):
+        self.frames = {}
+
+    def open_frame(self, firstThrow, secondThrow):
+        self.__add_frame__(OpenFrame(firstThrow, secondThrow))
+
+    def spare(self, firstThrow, secondThrow):
+        self.__add_frame__(SpareFrame(firstThrow, secondThrow))
+
+    def strike(self):
+        self.__add_frame__(StrikeFrame())
+
+    def __add_frame__(self, frame):
+        next_throw = self.__throws_count__() + 1
+        self.frames[next_throw] = frame
+
+        # if it is not the first throw
+        previous_throw = next_throw - 1
+        if previous_throw > 0:
+            previous_frame = self.frames[previous_throw]
+            if isinstance(previous_frame, FutureFrame):
+                previous_frame.next_frame = frame
+
+    def __throws_count__(self):
+        return len(self.frames.keys())
+
+    def score(self):
+        score = 0
+        for frame in self.frames.values():
+            score += frame.score()
+
+        return score
+
+class Frame:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    @abstractmethod
+    def score(self):
         pass
+
+class OpenFrame(Frame):
+
+    def score(self):
+        return self.first + self.second
+
+class FutureFrame(Frame):
+    __metaclass__ = ABCMeta
+    next_frame = None
+
+class SpareFrame(FutureFrame):
+
+    def score(self):
+        if self.next_frame:
+            return 10 + self.next_frame.first
+
+class StrikeFrame(FutureFrame):
+
+    def __init__(self):
+        self.first = 10
+        self.second = 0
+    
+    def score(self):
+        if self.next_frame:
+            return 10 + self.next_frame.first + self.next_frame.second
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -3,8 +3,48 @@ from app.bowling import *
 
 # Test the dependencies with unittest
 class BowlingTest(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BowlingGame()
+
     def test_hookup(self):
         self.assertTrue(True)
+
+    def test_gutter_balls(self):
+        self.__many_open_frames__(10,0,0)
+        self.assertEqual(0, self.game.score())
+
+    def test_threes(self):
+        self.__many_open_frames__(10,3,3)
+        self.assertEqual(60, self.game.score())
+
+    def test_spare_1(self):
+        game = self.game
+        game.spare(4,6)
+        game.open_frame(3,5)
+
+        self.__many_open_frames__(8,0,0)
+        self.assertEqual(21, self.game.score())
+
+    def test_spare_2(self):
+        game = self.game
+        game.spare(4,6)
+        game.open_frame(5,3)
+
+        self.__many_open_frames__(8,0,0)
+        self.assertEqual(23, self.game.score())
+
+    def test_strike(self):
+        game = self.game
+        game.strike()
+        game.open_frame(4,3)
+
+        self.__many_open_frames__(8,0,0)
+        self.assertEqual(24, game.score())
+
+    def __many_open_frames__(self, count, firstThrow, secondThrow):
+        for i in range(0, count):
+            self.game.open_frame(firstThrow, secondThrow)
 
 if __name__ == '__main__':
     unittest.main()
